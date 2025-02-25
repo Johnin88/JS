8555de7086cf1ef02252389d662a22f20a59aa09
johnin88 <johnatabezerra1@gmail.com>
2025-02-24 23:39:23 -0300

    Atualização da aula20

---
+++ JS/Aula20/atividade1.js
@@ -0,0 +1,9 @@
+// uma loja tem um estoque de 10 produtos. Os preços estão em um array de 10 posicoes
+// com o método forEach mostre no console:
+//  o preço original, o preço com desconto de 10% para pagamentos a vista e o preço com adicional da maquineta (para parcelamentos) de 15% a mais.
+
+let precosProdutos = [ 150, 280, 50, 100, 200, 300, 400, 500, 600, 700]
+
+function infoPrecos(valor){
+    
+}
