ce8da8e734c8977fd036da3d525465981c3dd107
johnin88 <johnatabezerra1@gmail.com>
2025-04-07 00:16:56 -0300

    Atualização de Aula 23

--- JS/Aula23/script.js
+++ JS/Aula23/script.js
@@ -1,17 +1,42 @@
-// Email
+// email (objeto)
 let email = document.getElementById('email')
+// mensagem (objeto)
+let mensagem = document.querySelector('#texto')
 
-// Mensagem
-let mensagem = document.querySelector('#mensagem')
-
-// Formulário - retirar o evneto pre definido
+// formulario - retirar o evento pre definido
 let formulario = document.querySelectorAll('form')[0]
 
-// Botão - adicionar o evneto de enviar o formulário
-let btn = getElementById('btnEnviar')
+// botão - para adicionar o evento de enviar o formulário 
+let btn = document.getElementById('btnEnviar')
 
-//antres de mostrar uma mensagem no alert verifique se existe uma mensagem no campo mensagem
+// antes de mostrar uma mensagem no alert verifique se existe uma mensagem no campo mensagem
 
-function verificarMensagem() {
-    let texto = mensagem.value // String
+function verificarMensagem(){
+    let texto = mensagem.value // string
+    if(texto.length > 0){
+        console.log('existe uma mensagem')
+        return true
+    }
+    else{
+        console.log('não existe uma mensagem')
+        return false
+    }
 }
+function enviarFormulario(){
+    let temMensagem = verificarMensagem()
+    if(temMensagem){
+        mensagem.value = ""
+        email.value = ""
+        alert('Mensagem enviada')
+    }
+    else{
+        alert('Digite uma mensagem')
+    }
+}
+btn.addEventListener('click', enviarFormulario)
+//remoção do evento pre definido do formulário 
+//evitando que todos os campos sejam apagados
+
+formulario.addEventListener('submit', (eventoEnviar)=>{
+    eventoEnviar.preventDefault()
+})
